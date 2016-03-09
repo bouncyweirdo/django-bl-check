c4ae6641fdbb3f0c3a17baf4f026da8f70264621
Valentin Olaru <vali@pidginhost.com>
2016-03-10 01:04:57 +0200

    update

--- blacklist_check/management/commands/check_ip_blacklist.py
+++ blacklist_check/management/commands/check_ip_blacklist.py
@@ -1,11 +1,6 @@
-import sys
-import os
-import socket
-import dns.resolver
-
 from django.core.management.base import BaseCommand
 
-from blacklist_check.models import IpAddress, DnsBlacklist
+from blacklist_check.models import IpAddress, Types
 from blacklist_check.utils import check_bl
 
 
@@ -13,6 +8,6 @@ class Command(BaseCommand):
     help = 'Check IP blacklist'
 
     def handle(self, *args, **options):
-        ips = IpAddress.objects.filter(enabled=True)
+        ips = IpAddress.objects.filter(enabled=True, status=Types.STATUS_ACTIVE)
         for ip in ips:
             check_bl(ip)
