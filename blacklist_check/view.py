277fd6890f3c5f161eac29dd6230fb71dc11a1b8
Valentin Olaru <vali@pidginhost.com>
2016-03-09 23:36:41 +0200

    update

--- blacklist_check/view.py
+++ blacklist_check/view.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
-@login_required(login_url='/blacklist/login')
+#@login_required(login_url='/blacklist/login')
 def index(request):
     data = dict()
     return render(request, "default/index.html", data)
