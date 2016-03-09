026d3ea547fa711038e0904c2776c5657aabeb52
Valentin Olaru <vali@pidginhost.com>
2016-03-09 22:58:45 +0200

    update

--- blacklist_check/urls.py
+++ blacklist_check/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from blacklist_check import view
 urlpatterns = patterns(
     '',
-    url(r'^index/$', view.index, name='index'),
-    url(r'^view/(?P<pk>[0-9]+)$', view.ip, name='cryptocoin-order-check-status'),
+    url(r'^index$', view.index, name='index'),
+    url(r'^view/(?P<pk>[0-9]+)$', view.ip, name='ip'),
 )
